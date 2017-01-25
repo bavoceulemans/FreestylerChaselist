b3283e6d74fa15da284dbb705aebf489a5cfdb56
Bavo <Bavo@MSI-BAVO>
2017-01-25 21:15:41 +0100

    Basic project reader. Open and Save button.

--- FreestylerChaselist/Form1.Designer.cs
+++ FreestylerChaselist/Form1.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openProjectButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.connectionStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.projectNameTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveProjectButton = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openProjectButton
+            // 
+            this.openProjectButton.Location = new System.Drawing.Point(255, 10);
+            this.openProjectButton.Name = "openProjectButton";
+            this.openProjectButton.Size = new System.Drawing.Size(57, 23);
+            this.openProjectButton.TabIndex = 1;
+            this.openProjectButton.Text = "Open";
+            this.openProjectButton.UseVisualStyleBackColor = true;
+            this.openProjectButton.Click += new System.EventHandler(this.openProjectButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectionStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(390, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // connectionStatusLabel
+            // 
+            this.connectionStatusLabel.Name = "connectionStatusLabel";
+            this.connectionStatusLabel.Size = new System.Drawing.Size(118, 17);
+            this.connectionStatusLabel.Text = "toolStripStatusLabel1";
+            // 
+            // projectNameTextbox
+            // 
+            this.projectNameTextbox.Location = new System.Drawing.Point(87, 12);
+            this.projectNameTextbox.Name = "projectNameTextbox";
+            this.projectNameTextbox.Size = new System.Drawing.Size(162, 20);
+            this.projectNameTextbox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Project name";
+            // 
+            // saveProjectButton
+            // 
+            this.saveProjectButton.Enabled = false;
+            this.saveProjectButton.Location = new System.Drawing.Point(318, 10);
+            this.saveProjectButton.Name = "saveProjectButton";
+            this.saveProjectButton.Size = new System.Drawing.Size(57, 23);
+            this.saveProjectButton.TabIndex = 5;
+            this.saveProjectButton.Text = "Save";
+            this.saveProjectButton.UseVisualStyleBackColor = true;
+            this.saveProjectButton.Click += new System.EventHandler(this.saveProjectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 261);
+            this.Controls.Add(this.saveProjectButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.projectNameTextbox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.openProjectButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button openProjectButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel connectionStatusLabel;
+        private System.Windows.Forms.TextBox projectNameTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveProjectButton;
     }
 }
 
--- FreestylerChaselist/Form1.cs
+++ FreestylerChaselist/Form1.cs
@@ -7,14 +7,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+//FreestylerConnection.dll added as a refrence. Methods are static
+using FreestylerConnection;
 
 namespace FreestylerChaselist
 {
     public partial class Form1 : Form
     {
+        public string rootFolder = "C:/Freestyler/Chases/";
+        public string projectName = "";
+
         public Form1()
         {
             InitializeComponent();
+
+            //Connect to FreeStyler and update label
+            connectionStatusLabel.Text = (FreestylerConnection.sender.freestylerConnected() != 0 ? "FS Connected" : "FS Not Found");
+        }
+
+        public void loadChase(int chasenr, string projectName, string chaseName, byte options)
+        {
+            int channel = chasenr % 2; //Even chases go into channel 0; Odd numbered into channel 1
+
+            string str = "0"+channel;
+            str += projectName + "/"; //Add the projectname to the str
+            str += chaseName; //Add the chaseName to the str
+            str += ".chb"; //Add the suffix
+            byte[] chaseValues = new byte[str.Length * sizeof(char)]; //The bytelist that has to be send
+            byte[] optionsValues = new byte[2];
+
+            //insert faderValues into correct position in array
+            for (int i = 0; i < str.Length; i++)
+            {
+                chaseValues[i] = (byte)str[i];
+            }
+
+            //set the options
+            optionsValues[0] = (byte)channel;
+            optionsValues[1] = options;
+
+            //send complated dmx values
+            FreestylerConnection.sender.sendMessageFunction(chaseValues, 5);
+            FreestylerConnection.sender.sendMessageFunction(optionsValues, 6);
+        }
+
+        private void openProjectButton_Click(object sender, EventArgs e)
+        {
+            projectName = projectNameTextbox.Text;
+
+            //Check if the chaselist folder exists
+            if (!Directory.Exists(""+rootFolder+projectName))
+            {
+                Directory.CreateDirectory("" + rootFolder + projectName);
+            }
+
+            //Check if the chaselist file exists
+            if (!File.Exists(""+rootFolder+projectName+"/"+projectName+".chl"))
+            {
+                File.Create("" + rootFolder + projectName + "/" + projectName + ".chl").Dispose();
+            } else
+            {
+                string path = rootFolder + projectName + "/" + projectName + ".chl";
+                string line = "";
+
+                using (System.IO.StreamReader file =
+                new System.IO.StreamReader(path))
+                {
+                    while((line = file.ReadLine()) != null)
+                    {
+                        MessageBox.Show(line);
+                    }
+                }
+            }
+
+            //Enable the saveButton
+            saveProjectButton.Enabled = true;
+        }
+
+        private void saveProjectButton_Click(object sender, EventArgs e)
+        {
+            //Check if there is a project
+            if (projectName != "")
+            {
+                string path = rootFolder + projectName + "/" + projectName + ".chl";
+
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(path))
+                {
+                    file.WriteLine("Lorem");
+                    file.WriteLine("Ipsum");
+                }
+            }
         }
     }
 }
