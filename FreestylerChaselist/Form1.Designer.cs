5febd014e5d7234355bd53146378ac094324d1d4
Bavo <Bavo@MSI-BAVO>
2017-01-25 23:27:30 +0100

    Cues can be loaded and played into Freestyler

--- FreestylerChaselist/Form1.Designer.cs
+++ FreestylerChaselist/Form1.Designer.cs
@@ -34,6 +34,13 @@
             this.projectNameTextbox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.saveProjectButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cueLabel = new System.Windows.Forms.Label();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,7 +60,7 @@
             this.connectionStatusLabel});
             this.statusStrip1.Location = new System.Drawing.Point(0, 239);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(390, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(554, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -90,18 +97,83 @@
             this.saveProjectButton.UseVisualStyleBackColor = true;
             this.saveProjectButton.Click += new System.EventHandler(this.saveProjectButton_Click);
             // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.LabelWrap = false;
+            this.listView1.Location = new System.Drawing.Point(12, 68);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(529, 97);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Tag = "1";
+            this.columnHeader1.Text = "Id";
+            this.columnHeader1.Width = 41;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Tag = "4";
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 86;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Tag = "6";
+            this.columnHeader3.Text = "Cue";
+            this.columnHeader3.Width = 268;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Tag = "3";
+            this.columnHeader4.Text = "AEL";
+            this.columnHeader4.Width = 73;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Tag = "2";
+            this.columnHeader5.Text = "Time";
+            this.columnHeader5.Width = 55;
+            // 
+            // cueLabel
+            // 
+            this.cueLabel.AutoSize = true;
+            this.cueLabel.Location = new System.Drawing.Point(15, 41);
+            this.cueLabel.Name = "cueLabel";
+            this.cueLabel.Size = new System.Drawing.Size(32, 13);
+            this.cueLabel.TabIndex = 7;
+            this.cueLabel.Text = "Cue: ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(390, 261);
+            this.ClientSize = new System.Drawing.Size(554, 261);
+            this.Controls.Add(this.cueLabel);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.saveProjectButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.projectNameTextbox);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.openProjectButton);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Freestyler Chaselist";
+            this.TopMost = true;
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -116,6 +188,13 @@
         private System.Windows.Forms.TextBox projectNameTextbox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button saveProjectButton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label cueLabel;
     }
 }
 
--- FreestylerChaselist/Form1.cs
+++ FreestylerChaselist/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.IO;
 //FreestylerConnection.dll added as a refrence. Methods are static
 using FreestylerConnection;
+using static System.Windows.Forms.ListViewItem;
 
 namespace FreestylerChaselist
 {
@@ -17,10 +18,21 @@ namespace FreestylerChaselist
     {
         public string rootFolder = "C:/Freestyler/Chases/";
         public string projectName = "";
+        System.Drawing.Font unselectedFont;
+        System.Drawing.Font selectedFont;
+        string[] loadedNames;
+        int rootChannel = 0;
 
         public Form1()
         {
             InitializeComponent();
+
+            //Create the fonts
+            unselectedFont = new System.Drawing.Font("Microsoft Sans Serif", 8, System.Drawing.FontStyle.Regular);
+            selectedFont = new System.Drawing.Font("Microsoft Sans Serif", 8, System.Drawing.FontStyle.Bold);
+
+            //Create empty array of loaded chases
+            loadedNames = new string[20];
 
             //Connect to FreeStyler and update label
             connectionStatusLabel.Text = (FreestylerConnection.sender.freestylerConnected() != 0 ? "FS Connected" : "FS Not Found");
@@ -28,7 +40,7 @@ namespace FreestylerChaselist
 
         public void loadChase(int chasenr, string projectName, string chaseName, byte options)
         {
-            int channel = chasenr % 2; //Even chases go into channel 0; Odd numbered into channel 1
+            int channel = rootChannel+(chasenr % 2); //Even chases go into channel 0; Odd numbered into channel 1
 
             string str = "0"+channel;
             str += projectName + "/"; //Add the projectname to the str
@@ -50,6 +62,9 @@ namespace FreestylerChaselist
             //send complated dmx values
             FreestylerConnection.sender.sendMessageFunction(chaseValues, 5);
             FreestylerConnection.sender.sendMessageFunction(optionsValues, 6);
+
+            //set the array
+            loadedNames[channel] = chaseName;
         }
 
         private void openProjectButton_Click(object sender, EventArgs e)
@@ -70,13 +85,26 @@ namespace FreestylerChaselist
             {
                 string path = rootFolder + projectName + "/" + projectName + ".chl";
                 string line = "";
+                listView1.Items.Clear();
+                loadedNames = new string[20];
 
                 using (System.IO.StreamReader file =
                 new System.IO.StreamReader(path))
                 {
                     while((line = file.ReadLine()) != null)
                     {
-                        MessageBox.Show(line);
+                        string[] args = line.Split(';');
+
+                        byte optionsByte = byte.Parse(args[3]);
+                        string optionsString = "";
+                        if (IsBitSet(optionsByte, 3)) optionsString += "A"; else optionsString += ".";
+                        if (IsBitSet(optionsByte, 2)) optionsString += "E"; else optionsString += ".";
+                        if (IsBitSet(optionsByte, 1)) optionsString += "L"; else optionsString += ".";
+
+                        string timeString = args[4] + "ms";
+
+                        string[] row = { args[1], args[2], optionsString, timeString };
+                        listView1.Items.Add(args[0]).SubItems.AddRange(row);
                     }
                 }
             }
@@ -98,6 +126,86 @@ namespace FreestylerChaselist
                     file.WriteLine("Lorem");
                     file.WriteLine("Ipsum");
                 }
+            }
+        }
+
+        public bool IsBitSet(byte b, int pos)
+        {
+            return (b & (1 << pos)) != 0;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            playSelectedChase();
+        }
+
+        public void playSelectedChase()
+        {
+            //Reset the colors
+            resetListViewColors();
+
+            ListView.SelectedListViewItemCollection args = this.listView1.SelectedItems;
+
+            foreach (ListViewItem chase in args)
+            {
+                //Set the color
+                chase.BackColor = Color.Green;
+                chase.Font = selectedFont;
+
+                //Get the chase info
+                int chasenr = int.Parse(chase.SubItems[0].Text);
+                string chaseName = chase.SubItems[1].Text;
+                string cue = chase.SubItems[2].Text;
+                string optionsString = chase.SubItems[3].Text;
+                int time = int.Parse(chase.SubItems[4].Text.Substring(0, (chase.SubItems[4].Text.Length - 2)));
+
+                int channel = rootChannel+(chasenr % 2); //Even chases go into channel 0; Odd numbered into channel 1
+                int nextChannel = rootChannel + ((chasenr + 1) % 2); //Even chases go into channel 0; Odd numbered into channel 1
+
+                //Set the cue label
+                cueLabel.Text = "Cue: " + cue;
+
+                //Check if the chase is loaded
+                if (loadedNames[channel] != chaseName)
+                {
+                    //Load the chase
+                    loadChase(chasenr, projectName, chaseName, 0);
+                }
+
+                //Preload the next chase
+                if (listView1.SelectedIndices[0] < listView1.Items.Count - 1)
+                {
+                    //Get the next index
+                    int nextIndex = listView1.SelectedIndices[0] + 1;
+
+                    //Get the name of the next chase
+                    string nextChaseName = listView1.Items[nextIndex].SubItems[1].Text;
+
+                    //Check if the chase is loaded
+                    if (loadedNames[nextChannel] != nextChaseName)
+                    {
+                        //Load the chase
+                        loadChase(nextIndex, projectName, nextChaseName, 0);
+                    }
+                }
+
+                //Play the chase
+                FreestylerConnection.sender.sendCommandFunction(505 + channel, 1); //Start
+                FreestylerConnection.sender.sendCommandFunction(525 + nextChannel, 1); //Stop
+            }
+        }
+
+        private void resetListViewColors()
+        {
+            foreach (ListViewItem chase in listView1.Items)
+            {
+                chase.BackColor = Color.White;
+                chase.Font = unselectedFont;
             }
         }
     }
